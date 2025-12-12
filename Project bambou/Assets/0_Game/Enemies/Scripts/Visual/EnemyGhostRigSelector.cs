b78a88d228274f4f50e6cf49f040d87afb90c36f
GuillaumeW <Guillaume.wiart@outlook.fr>
2025-12-12 09:59:11 +0100

    OMG ghost rig ??

--- Project bambou/Assets/0_Game/Enemies/Scripts/Visual/EnemyGhostRigSelector.cs
+++ Project bambou/Assets/0_Game/Enemies/Scripts/Visual/EnemyGhostRigSelector.cs
@@ -5,12 +5,13 @@ namespace Enemies.Visual
     public class EnemyGhostRigSelector : MonoBehaviour
     {
         private EnemyPoseFollower follower;
+        [SerializeField] private string enemyID;
 
         void Start()
         {
             follower = GetComponent<EnemyPoseFollower>();
 
-            var ghost = EnemyGhostRigSystem.Instance.GetGhostRigs[Random.Range(0, EnemyGhostRigSystem.Instance.GetGhostRigs.Length)];
+            var ghost = EnemyGhostRigSystem.Instance.GetGhostRigs[enemyID][Random.Range(0, EnemyGhostRigSystem.Instance.GetGhostRigs[enemyID].Length)];
             follower.SetGhostRig(ghost);
         }
     }
--- Project bambou/Assets/0_Game/Enemies/Scripts/Visual/EnemyGhostRigSystem.cs
+++ Project bambou/Assets/0_Game/Enemies/Scripts/Visual/EnemyGhostRigSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,7 +6,7 @@ namespace Enemies.Visual
 {
     public class EnemyGhostRigSystem : MonoBehaviour
     {
-        [SerializeField] private Transform[] ghostRigAnimators;
+        [SerializeField] private SerializableDictionary<string, Transform[]> ghostRigAnimators;
         
         #region Singleton
 
@@ -18,6 +19,54 @@ namespace Enemies.Visual
         
         #endregion
 
-        public Transform[] GetGhostRigs => ghostRigAnimators;
+        public Dictionary<string, Transform[]> GetGhostRigs => ghostRigAnimators;
+    }
+    
+    [Serializable] //TODO Dégager cette merde d'ici pleaaaaaasssssseeeeee
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField] private List<Entry> entries = new List<Entry>();
+
+        [Serializable]
+        private struct Entry
+        {
+            public TKey key;
+            public TValue value;
+        }
+
+        // Avant que Unity sérialise l'objet (Editor -> disque, playmode, etc.)
+        public void OnBeforeSerialize()
+        {
+            entries.Clear();
+            foreach (var kvp in this)
+            {
+                entries.Add(new Entry { key = kvp.Key, value = kvp.Value });
+            }
+        }
+
+        // Après que Unity a désérialisé l'objet
+        public void OnAfterDeserialize()
+        {
+            Clear();
+
+            for (int i = 0; i < entries.Count; i++)
+            {
+                var key = entries[i].key;
+
+                // Attention: si clé dupliquée, on garde la dernière (ou ignore, à choix)
+                if (ContainsKey(key))
+                {
+                    // Option 1: écraser
+                    this[key] = entries[i].value;
+
+                    // Option 2: ignorer (décommentez si vous préférez)
+                    // continue;
+                }
+                else
+                {
+                    Add(key, entries[i].value);
+                }
+            }
+        }
     }
 }
