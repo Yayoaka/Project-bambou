9f269c03ca7096880f5bb67f6f03d58d65482fae
Yayoaka <76622183+Yayoaka@users.noreply.github.com>
2025-12-12 10:19:21 +0100

    abomination second enemy

---
+++ Project bambou/Assets/1_Scenes/Asset Scene/RandomPositionZone.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class RandomPositionZone : MonoBehaviour
+{
+    public float firstRadius = 150f;
+    public float secondRadius = 200f;
+    public float maxRadius = 250f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        DrawCircle(transform.position, firstRadius);
+        
+        Gizmos.color = Color.green;
+        DrawCircle(transform.position, secondRadius);
+
+        Gizmos.color = Color.red;
+        DrawCircle(transform.position, maxRadius);
+    }
+
+    private void DrawCircle(Vector3 center, float radius, int segments = 100)
+    {
+        Vector3 prevPoint = center + new Vector3(Mathf.Cos(0f) * radius, 0f, Mathf.Sin(0f) * radius);
+
+        for (int i = 1; i <= segments; i++)
+        {
+            float angle = i * 2f * Mathf.PI / segments;
+            Vector3 newPoint = center + new Vector3(Mathf.Cos(angle) * radius, 0f, Mathf.Sin(angle) * radius);
+
+            Gizmos.DrawLine(prevPoint, newPoint);
+            prevPoint = newPoint;
+        }
+    }
+}
